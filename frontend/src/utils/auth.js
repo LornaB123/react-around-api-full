d8237daa0f597f8f44ef17f43fe6bbd602dfa152
Lorna B <lorna.bogan@gmail.com>
2021-05-14 16:01:38 -0500

    update frontend localhost api/auth

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'https://register.nomoreparties.co'
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3001';
 
 export const register = (password, email) => {
     return fetch(`${BASE_URL}/signup`, {
